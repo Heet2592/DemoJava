f09a401d78d95433c4f6825f6b988d2553267a5a
Heet Patel <heetpatel2592@gmail.com>
2022-11-17 22:02:43 -0500

    First Commit

---
+++ JavaDemoApp/src/test/code/FirstProgram.java
@@ -0,0 +1,10 @@
+package test.code;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!");
+	}
+
+}
