86c739a21d0f95e754fe2a2197757e7874c6608c
Heet Patel <heetpatel2592@gmail.com>
2022-11-18 23:04:06 -0500

    First Commit

--- JavaDemoApp/src/test/code/FirstProgram.java
+++ JavaDemoApp/src/test/code/FirstProgram.java
@@ -5,6 +5,15 @@ public class FirstProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!!");
+		
+		String s1 = "Dhoni";
+		int lenght = s1.toLowerCase().length();
+		System.out.println(lenght);
+		
+		StringBuffer sb = new StringBuffer("Sachin");
+		
+		int lenght1  = sb.reverse().length();
+		System.out.println(lenght1);
 	}
 
 }
